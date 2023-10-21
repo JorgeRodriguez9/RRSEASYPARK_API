dd2dcfac100fa4257580e95d215ba335a4cfae23
JorgeRodriguez9 <131720274+JorgeRodriguez9@users.noreply.github.com>
2023-10-21 02:30:13 -0500

    Logic Reservation

--- RRSEASYPARK/Service/ReservationService.cs
+++ RRSEASYPARK/Service/ReservationService.cs
@@ -1,4 +1,6 @@
-﻿using FluentAssertions.Extensions;
+﻿using FluentAssertions.Common;
+using FluentAssertions.Extensions;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using RRSEasyPark.Models;
 using RRSEASYPARK.DAL;
@@ -15,7 +17,7 @@ namespace RRSEASYPARK.Service
         {
             _context = context;
         }
-        public async Task<ServiceResponse> AddReservation(string date, long totalprice,TimeOnly starttime, TimeOnly endtime, Guid typeVehicleId, Guid parkingLotId, string disability)
+        public async Task<ServiceResponse> AddReservation(string date, long totalprice, TimeOnly starttime, TimeOnly endtime, Guid typeVehicleId, Guid parkingLotId, string disability)
         {
             try
             {
@@ -23,7 +25,7 @@ namespace RRSEASYPARK.Service
 
                 var ParkingLot = await _context.parkingLots.FindAsync(parkingLotId);
                 var TypeVehicle = await _context.typeVehicles.FindAsync(typeVehicleId);
-                
+
 
                 if (ParkingLot == null)
                 {
@@ -35,14 +37,68 @@ namespace RRSEASYPARK.Service
                     throw new Exception("TypeVehicle is null");
                 }
 
+                var test = 1;
 
+                switch (TypeVehicle.Name)
+                {
+                    case "Carro":
+                        if (disability == Enums.DisabilityValues.SI.ToString())
+                        {
+                            //ParkingLot.CantSpacesCar += (int)Enums.NumbersValues.b;
+                            if (ParkingLot.CantSpacesDisability <= (int)Enums.NumbersValues.a)
+                            {
+                                //return new ServiceResponse()
+                                //{
+                                //    Result = ServiceResponseType.Failed,
+                                //    ErrorMessage = "there are no available spaces"
+                                //};
+                                test = 0;
+                            }
+                            break;
+                        }
+                        if (ParkingLot.CantSpacesCar <= (int)Enums.NumbersValues.a)
+                        {
+                            //return new ServiceResponse()
+                            //{
+                            //    Result = ServiceResponseType.Failed,
+                            //    ErrorMessage = "there are no available spaces"
+                            //};
+                            test = 0;
+                        }
+                        break;
+                    case "Moto":
+                        if (ParkingLot.CantSpacesMotorcycle <= (int)Enums.NumbersValues.a)
+                        {
+                            //return new ServiceResponse()
+                            //{
+                            //    Result = ServiceResponseType.Failed,
+                            //    ErrorMessage = "there are no available spaces"
+                            //};
+                            test = 0;
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Unrecognized vehicle type");
+                        break;
+                }
+
+                var Reservations = await _context.reservations.Where(x => x.ParkingLotId == parkingLotId && x.Date == date).ToListAsync();
+
+                if (!ValidateReservation(Reservations, starttime, endtime, test))
+                {
+                    return new ServiceResponse()
+                    {
+                        Result = ServiceResponseType.Failed,
+                        ErrorMessage = "Time span not available"
+                    };
+                }
 
                 await _context.reservations.AddAsync(new Reservation()
                 {
                     Id = Guid.NewGuid(),
                     //Date = TimeZoneInfo.ConvertTime(date, colombiaTimeZone),
                     Date = date.ToString(),
-                    StartTime =starttime.ToString(),
+                    StartTime = starttime.ToString(),
                     EndTime = endtime.ToString(),
                     TypeVehicleId = typeVehicleId,
                     ParkingLotId = parkingLotId,
@@ -53,7 +109,7 @@ namespace RRSEASYPARK.Service
 
                 switch (TypeVehicle.Name)
                 {
-                    case "Carro":                        
+                    case "Carro":
                         if (disability == Enums.DisabilityValues.SI.ToString())
                         {
                             //ParkingLot.CantSpacesCar += (int)Enums.NumbersValues.b;
@@ -99,7 +155,7 @@ namespace RRSEASYPARK.Service
         {
             return await _context.reservations.Include(x => x.ClientParkingLot).ToListAsync();
         }
-        public async Task<ServiceResponse> UpdateReservation(Guid ReservationId, string date,TimeOnly startTime, TimeOnly endTime, long totalPrice, string disabled)
+        public async Task<ServiceResponse> UpdateReservation(Guid ReservationId, string date, TimeOnly startTime, TimeOnly endTime, long totalPrice, string disabled)
         {
             try
             {
@@ -171,5 +227,37 @@ namespace RRSEASYPARK.Service
                 };
             }
         }
+
+        public bool ValidateReservation(IEnumerable<Reservation> reservations, TimeOnly starttime, TimeOnly endtime, int test)
+        {
+            if (reservations.Count() == 0)
+            {
+                return true;
+            }
+
+            if (test == 1)
+            {
+                return true;
+            }
+
+            foreach (var Reservation in reservations)
+            {
+                var StartT = TimeOnly.Parse(Reservation.StartTime);
+                var EndT = TimeOnly.Parse(Reservation.EndTime);
+
+                var V1 = starttime >= StartT; // si
+                var V2 = endtime >= StartT;   // si    true
+
+                var V3 = endtime <= EndT;    //no
+                var V4 = starttime <= EndT;   //no    true
+
+                if (V1 == V2 && V3 == V4 && (V1 != V3 && V1 != V4) && (V2 != V3 && V2 != V4))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
     }
 }
