309e4c58cc7e2c71629720d6e00246002f926c2c
JorgeRodriguez9 <131720274+JorgeRodriguez9@users.noreply.github.com>
2023-10-23 23:33:01 -0500

    Setting timeOnly to string

--- RRSEASYPARK/Service/ReservationService.cs
+++ RRSEASYPARK/Service/ReservationService.cs
@@ -156,7 +156,7 @@ namespace RRSEASYPARK.Service
         {
             return await _context.reservations.Include(x => x.ClientParkingLot).ToListAsync();
         }
-        public async Task<ServiceResponse> UpdateReservation(Guid ReservationId, string date, TimeOnly startTime, TimeOnly endTime, long totalPrice, string disabled)
+        public async Task<ServiceResponse> UpdateReservation(Guid ReservationId, string date, string startTime, string endTime, long totalPrice, string disabled)
         {
             try
             {
